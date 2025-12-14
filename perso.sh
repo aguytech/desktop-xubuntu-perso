db339d0a34ca5295a48483b25c420420cbcb116a
aguy tech <aguytech@free.fr>
2025-12-14 14:00:33 +0100

    Init from ubuntu 24.04.5 66bf393

---
+++ perso.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+######################## CONF
+
+S_TRACE=debug
+_PATH_BASE=$( readlink -f ${0%/*} )
+_PATH_CONF=/usr/local/conf
+_PATH_LOG=/var/log/desktop-install
+_VER_NAME=ubuntu
+_path_base=$( readlink -f ${0%/*}/../ )
+
+# inc
+file=${_path_base}/bs/inc
+! [ -f "${file}" ] && echo "Unable to find file: ${file}" && exit 1
+! . ${file} && echo "Errors while sourcing file: ${file}" && exit 1
+
+########################  WWW
+
+sudo ping -c1 google.com >/dev/null 2>&1 || _exite "Installation needs internet connection"
+
+########################  VARIABLES
+
+export DEBIAN_FRONTEND=noninteractive
+
+########################  SUB
+
+_SPATH=perso
+parts_perso="python libreoffice conf launcher git root"
+for _PART in ${parts_perso}; do
+	_source_sub "${_PART}" ${_SPATH}
+done
+
+parts_install=$( ls ${_PATH_BASE}/${_SPATH} )
+
+while [ "${_PART}" != "quit" ]; do
+	_SDATE=$(date +%s) # renew _SDATE
+	parts_made=" $( grep "^${_SPATH}_" "${S_FILE_INSTALL_DONE}" | cut -d'_' -f2 | xargs ) "
+	parts2do=" "
+	for part in ${parts_install}; do
+		[ "${parts_made/ ${part} }" = "${parts_made}" ] && parts2do+="$part "
+	done
+
+	_echod "parts_made='${parts_made}'"
+	_echod "parts2do='${parts2do}'"
+
+	[ "${parts_made}" ] && _echo "Part already made: ${cyanb}${parts_made}${cclear}"
+	PS3="Give your choice: "
+	select _PART in quit ${parts2do}; do
+		if [ "${parts2do/ ${_PART} /}" != "${parts2do}" ] ; then
+			_source_sub ${_PART} ${_SPATH}
+			break
+		elif [ "${_PART}" = quit ]; then
+			break
+		else
+			_echoe "Wrong option"
+		fi
+	done
+done
+
+_exit
